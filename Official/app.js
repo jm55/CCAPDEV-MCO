9c989b066b8470254b7c3b212c6256cc74e261f7
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2022-05-16 16:19:50 +0800

    Added pre-made app.js from lecture

--- Official/app.js
+++ Official/app.js
@@ -1,2 +1,34 @@
 console.log("App.js");
 
+//Import modules
+const express = require('express');
+const hbs = require('express-handlebars');
+
+//Create server object
+const app = express();
+
+//PORT NUMBER
+const PORT = process.env.PORT || 3000;
+
+//Set static folder
+app.use(express.static(__dirname + "/public"));
+
+//Set hbs as view engine
+app.engine("hbs", hbs.engine({extname: "hbs"}));
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+//Set view cache
+app.set("view cache", false);
+
+//App Listens
+app.listen(PORT,()=>{
+    console.log("localhost:" + PORT);
+});
+
+//Index Route
+app.get("/", (req, res)=>{
+    res.sendStatus(200);
+});
+
+//Other Routes
