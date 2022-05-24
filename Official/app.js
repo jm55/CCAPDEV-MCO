125979740e21c1a9e18157999d9e27c140ea7399
Miguel Escalona - DLSU <71485171+jm55DLSU@users.noreply.github.com>
2022-05-24 09:31:42 +0800

    Added jquery, mongodb, and es6 on package
    Please execute the run_install.bat to update dependencies/node modules installed on your machine

--- Official/app.js
+++ Official/app.js
@@ -1,6 +1,11 @@
-const path = require('path');
-const express = require('express');
-const hbs = require('express-handlebars');
+//Path __dirname fix: https://flaviocopes.com/fix-dirname-not-defined-es-module-scope/
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+import express from 'express';
+import hbs from 'express-handlebars';
 
 const PORT = process.env.PORT || 3000;
 
@@ -19,14 +24,21 @@ app.get('/', (req, res)=>{
     //res.status(200).send("Hello World"); //SEND
     //res.status(200).json({"message":"Hello World"}); //SEND JSON FORMAT 
     //res.status(200).download("app.js"); //DOWNLOAD FILE
+    console.log('index redirecting...');
     res.redirect('/login');
 });
+
+import utils from './utils.js';
 
 app.get('/login', (req,res)=>{
     res.render("login", {title: "Login - Budol Finds"}); 
 });
 
 app.get('/home', (req, res)=>{
+    utils.autoFill();
+    console.log(utils.users);
+    console.log(utils.posts);
+    console.log(utils.comments);
     res.render("home",  {title: "Home - Budol Finds"});
 });
 
